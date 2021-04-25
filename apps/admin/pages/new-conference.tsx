993e29c12cbb053e6e991d85a735ee4095c26e03
Pablo Rodríguez Guillén <pablorodriguezguillen@gmail.com>
2021-04-25 19:59:21 +0200

    feat(admin): add validation behaviour

--- apps/admin/pages/new-conference.tsx
+++ apps/admin/pages/new-conference.tsx
@@ -1,24 +1,45 @@
 import React from 'react';
+import { useForm } from 'react-hook-form';
 
-export default function CreateConference() {
+type Props = {
+  onCreateConference: () => Promise<void>;
+};
+
+type FormValues = {
+  name: string;
+  url: string;
+  place: string;
+  startDate: Date;
+  endDate: Date;
+  logoFile: FileList;
+};
+
+const CreateConference: React.FunctionComponent<Props> = ({ onCreateConference }) => {
+  const { handleSubmit, register } = useForm<FormValues>();
+
   return (
-    <>
+    <form onSubmit={handleSubmit(onCreateConference)}>
       <label>
-        Nombre de la conferencia <input type="text" />
+        Nombre de la conferencia <input {...register('name', { required: true })} type="text" />
       </label>
       <label>
-        Enlace para la página principal <input type="text" />
+        Enlace para la página principal <input {...register('url', { required: true })} type="text" />
       </label>
       <label>
-        Lugar de celebración <input type="text" />
+        Lugar de celebración <input {...register('place')} type="text" />
       </label>
       <label>
-        Fecha de celebración <input type="date" />
+        Fecha de inicio <input {...register('startDate')} type="date" />
       </label>
       <label>
-        Logo de conferencia <input type="file" />
+        Fecha de fin <input {...register('endDate')} type="date" />
       </label>
-      <button>Crear conferencia</button>
-    </>
+      <label>
+        Logo de conferencia <input {...register('logoFile')} type="file" />
+      </label>
+      <input type="submit" value="Crear conferencia" />
+    </form>
   );
-}
+};
+
+export default CreateConference;
