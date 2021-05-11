37e2d94ae0a93bd3ba3bd20de7878e01dc3ede17
Pablo Rodríguez Guillén <pablorodriguezguillen@gmail.com>
2021-05-11 19:31:40 +0200

    fix(api): change conference place error message

--- apps/api/src/conference/domain/model/conference-place.ts
+++ apps/api/src/conference/domain/model/conference-place.ts
@@ -3,7 +3,7 @@ import { ValueObject } from '@meridio/domain';
 export class ConferencePlace extends ValueObject<{ value: string }> {
   public static fromString(place: string) {
     if (place.length === 0) {
-      throw new Error('Conference url cannot be empty');
+      throw new Error('Conference place cannot be empty');
     }
 
     return new ConferencePlace({ value: place });
