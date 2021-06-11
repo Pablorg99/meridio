293b62c13468ef84bc2bc1d1fa28e93f7b253f2b
Pablo Rodríguez Guillén <pablorodriguezguillen@gmail.com>
2021-06-11 19:05:58 +0200

    refactor(admin-e2e): use create conference dto mother

--- apps/admin-e2e/src/integration/CreateConference.spec.ts
+++ apps/admin-e2e/src/integration/CreateConference.spec.ts
@@ -1,24 +1,20 @@
-import * as faker from 'faker';
+import { CreateConferenceDTOMother } from '@meridio/contracts';
 
 describe('CreateConference', () => {
   before(() => {
     cy.visit('/conference/new');
   });
 
-  const conferenceName = faker.random.word();
-  const conferenceUrl = `${faker.internet.url()}/${faker.datatype.number()}`;
-  const conferencePlace = faker.random.word();
-  const startDate = faker.date.soon().toISOString().split('T')[0];
-  const endDate = faker.date.future().toISOString().split('T')[0];
+  const conference = CreateConferenceDTOMother.random()
 
   it('filling the form creates a conference', () => {
     cy.intercept('POST', '/conferences').as('createConference');
 
-    cy.findByRole('textbox', { name: 'Nombre de la conferencia' }).type(conferenceName);
-    cy.findByRole('textbox', { name: 'Enlace para la página principal' }).type(conferenceUrl);
-    cy.findByRole('textbox', { name: 'Lugar de celebración' }).type(conferencePlace);
-    cy.findByLabelText('Fecha de inicio').type(startDate);
-    cy.findByLabelText('Fecha de fin').type(endDate);
+    cy.findByRole('textbox', { name: 'Nombre de la conferencia' }).type(conference.name);
+    cy.findByRole('textbox', { name: 'Enlace para la página principal' }).type(conference.url);
+    cy.findByRole('textbox', { name: 'Lugar de celebración' }).type(conference.place);
+    cy.findByLabelText('Fecha de inicio').type(conference.startDate);
+    cy.findByLabelText('Fecha de fin').type(conference.endDate);
     cy.findByRole('button', { name: 'Crear conferencia' }).click();
 
     cy.wait('@createConference').its('response.statusCode').should('be.equal', 201);
