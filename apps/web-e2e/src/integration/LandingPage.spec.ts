46df9072f214e056581ca8aa37bd288385236449
Pablo Rodríguez Guillén <pablorodriguezguillen@gmail.com>
2021-09-11 20:22:53 +0200

    refactor(web): rename conferenceUrl to conferenceSlug

--- apps/web-e2e/src/integration/LandingPage.spec.ts
+++ apps/web-e2e/src/integration/LandingPage.spec.ts
@@ -2,18 +2,18 @@ import { CreateConferenceDTO, CreateConferenceDTOMother } from '@meridio/contrac
 import * as faker from 'faker';
 
 describe('LandingPage', function () {
-  let conferenceUrl: string;
+  let conferenceSlug: string;
   let conference: CreateConferenceDTO;
 
   before(() => {
-    conferenceUrl = faker.random.word();
-    conference = CreateConferenceDTOMother.withLandingOpenAndUrl(conferenceUrl);
+    conferenceSlug = faker.random.word();
+    conference = CreateConferenceDTOMother.withLandingOpenAndUrl(conferenceSlug);
   });
 
   it('should show the conference info when visiting the landing page', function () {
     cy.request('POST', 'http://localhost:3333/api/conferences', conference);
 
-    cy.visit(`/${conferenceUrl}`);
+    cy.visit(`/${conferenceSlug}`);
 
     cy.findByText(conference.name);
     cy.findByText(conference.slug);
