8b4cf36b0d3ebabf848ae1b15cab36ae09b9d9cb
Pablo Rodríguez Guillén <pablorodriguezguillen@gmail.com>
2021-10-10 16:09:42 +0200

    feat(web): buy ticket

---
+++ apps/web-e2e/src/integration/BuyTicket.spec.ts
@@ -0,0 +1,23 @@
+import { CreateConferenceDTOMother, CreateTicketDTOMother } from '@meridio/contracts';
+import * as faker from 'faker';
+
+describe('Buy ticket', function() {
+  const conference = CreateConferenceDTOMother.withLandingOpenAndUrl(faker.random.word());
+  const ticket = CreateTicketDTOMother.random();
+
+  before(() => {
+    cy.request('POST', 'http://localhost:3333/api/conferences', conference);
+  });
+
+  it('should buy a ticket', function() {
+    cy.intercept('POST', '/tickets').as('createTicket');
+    cy.visit(`${conference.slug}/ticket`);
+
+    cy.findByRole('textbox', { name: 'Nombre completo' }).type(ticket.assistantInfo.fullName);
+    cy.findByRole('textbox', { name: 'Email' }).type(ticket.assistantInfo.email);
+    cy.findByRole('button', { name: 'Adquirir entrada' }).click();
+
+    cy.wait('@createTicket').its('response.statusCode').should('be.equal', 201);
+    cy.url().should('include', `/${conference.slug}/ticket`)
+  });
+});
--- apps/web/pages/[conferenceSlug]/ticket.tsx
+++ apps/web/pages/[conferenceSlug]/ticket.tsx
@@ -2,28 +2,35 @@ import { ConferenceDTO, CreateTicketDTO } from '@meridio/contracts';
 import { BuyTicketComponent, BuyTicketFormData } from '@meridio/ui';
 import axios from 'axios';
 import { useRouter } from 'next/dist/client/router';
-import React, { useCallback } from 'react';
+import React, { useCallback, useEffect, useState } from 'react';
 import * as uuid from 'uuid';
 
-export default async function BuyTicket() {
+export default function BuyTicket() {
   const router = useRouter();
   const { conferenceSlug } = router.query;
 
-  const { data: conference } = await axios.get<ConferenceDTO>(
-    `http://localhost:3333/api/conferences/landings/${conferenceSlug}`
-  );
+  const [conference, setConference] = useState<ConferenceDTO>();
+
+  useEffect(() => {
+    if (conferenceSlug) {
+      axios.get<ConferenceDTO>(`http://localhost:3333/api/conferences/landings/${conferenceSlug}`).then((response) => {
+        setConference(response.data);
+      });
+    }
+  }, [conferenceSlug]);
 
   const onBuyTicket = useCallback(
     async (data: BuyTicketFormData) => {
-      const body: CreateTicketDTO = {
-        id: uuid.v4(),
-        conferenceId: conference.id,
-        assistantInfo: data,
-      };
-      await axios.post('http://localhost:3333/api/tickets', body);
-      await router.push(`/${conferenceSlug}`);
+      if (conference?.id) {
+        const body: CreateTicketDTO = {
+          id: uuid.v4(),
+          conferenceId: conference.id,
+          assistantInfo: data,
+        };
+        await axios.post('http://localhost:3333/api/tickets', body);
+      }
     },
-    [conference.id, conferenceSlug, router]
+    [conference?.id, conferenceSlug, router]
   );
 
   return <BuyTicketComponent onBuyTicket={onBuyTicket} />;
