0b5af5c594084911ef5f0c2314d1699534f46262
Pablo Rodríguez Guillén <pablorodriguezguillen@gmail.com>
2021-10-24 19:50:17 +0200

    refactor(api): remove unused code

--- apps/api/src/bootstrap.module.ts
+++ apps/api/src/bootstrap.module.ts
@@ -1,14 +1,11 @@
 import { Module } from '@nestjs/common';
 import { APP_GUARD } from '@nestjs/core';
-import { TypeOrmModule } from '@nestjs/typeorm';
 import { EventSourcingModule } from 'event-sourcing-nestjs';
 
 import { RolesGuard } from './auth/security/roles.guard';
-import { configService } from './config/config.service';
 
 @Module({
   imports: [
-    TypeOrmModule.forRoot(configService.getTypeOrmConfig()),
     EventSourcingModule.forRoot({
       mongoURL: process.env.MONGO_EVENT_STORE_URI,
     }),
