e0dc016ee72b7e17f0f9593fad3cbfcad463d124
Pablo Rodríguez Guillén <pablorodriguezguillen@gmail.com>
2021-10-31 18:49:59 +0100

    feat(web): login page as home

--- apps/web/pages/_app.tsx
+++ apps/web/pages/_app.tsx
@@ -1,29 +1,14 @@
 import { ChakraProvider } from '@chakra-ui/react';
-import { theme } from '@meridio/ui';
 import { AppProps } from 'next/app';
-import { Provider as NextAuthProvider } from 'next-auth/client';
-import PropTypes from 'prop-types';
+import { Provider as SessionProvider } from 'next-auth/client';
 import React from 'react';
 
 export default function MyApp({ Component, pageProps }: AppProps) {
-  React.useEffect(() => {
-    // Remove the server-side injected CSS.
-    const jssStyles = document.querySelector('#jss-server-side');
-    if (jssStyles) {
-      jssStyles.parentElement?.removeChild(jssStyles);
-    }
-  }, []);
-
   return (
-    <NextAuthProvider session={pageProps.session}>
+    <SessionProvider session={pageProps.session}>
       <ChakraProvider>
         <Component {...pageProps} />
       </ChakraProvider>
-    </NextAuthProvider>
+    </SessionProvider>
   );
 }
-
-MyApp.propTypes = {
-  Component: PropTypes.elementType.isRequired,
-  pageProps: PropTypes.object.isRequired,
-};
--- apps/web/pages/index.tsx
+++ apps/web/pages/index.tsx
@@ -1,22 +1,12 @@
-import Box from '@material-ui/core/Box';
-import Container from '@material-ui/core/Container';
-import Typography from '@material-ui/core/Typography';
-import { Layout } from '@meridio/ui';
-import { useSession } from 'next-auth/client';
+import { LogIn } from '@meridio/ui';
+import { signIn, signOut, useSession } from 'next-auth/client';
 import React from 'react';
 
 export default function Index() {
-  const [session, loading] = useSession();
+  const [session] = useSession();
 
-  return (
-    <Layout session={session}>
-      <Container maxWidth="sm">
-        <Box my={4}>
-          <Typography variant="h4" component="h1" gutterBottom>
-            Next.js example
-          </Typography>
-        </Box>
-      </Container>
-    </Layout>
-  );
+  const logIn = () => signIn('github');
+  const logOut = () => signOut();
+
+  return <LogIn logIn={logIn} logOut={logOut} session={session} />;
 }
