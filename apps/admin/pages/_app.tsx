1eb525db9abd027bd9d1d00b069cc08154eae518
Pablo Rodríguez Guillén <pablorodriguezguillen@gmail.com>
2021-10-31 18:48:52 +0100

    fix(admin): add provider for useSession context across app

--- apps/admin/pages/_app.tsx
+++ apps/admin/pages/_app.tsx
@@ -1,6 +1,7 @@
 import { ChakraProvider } from '@chakra-ui/react';
 import { AppProps } from 'next/app';
 import Head from 'next/head';
+import { Provider as SessionProvider } from 'next-auth/client';
 import React from 'react';
 
 function App({ Component, pageProps }: AppProps) {
@@ -10,9 +11,11 @@ function App({ Component, pageProps }: AppProps) {
         <title>Meridio Admin</title>
         <link rel="shortcut icon" href="https://i.ibb.co/1bLY5Ht/logo.png" />
       </Head>
-      <ChakraProvider>
-        <Component {...pageProps} />
-      </ChakraProvider>
+      <SessionProvider session={pageProps.session}>
+        <ChakraProvider>
+          <Component {...pageProps} />
+        </ChakraProvider>
+      </SessionProvider>
     </>
   );
 }
