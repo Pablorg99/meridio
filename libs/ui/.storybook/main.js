f91afde9f693d4ad372148640b6259317094f141
Pablo Rodríguez Guillén <pablorodriguezguillen@gmail.com>
2021-11-21 19:51:06 +0100

    chore: remove storybook

--- libs/ui/.storybook/main.js
+++
@@ -1,8 +0,0 @@
-// eslint-disable-next-line @typescript-eslint/no-var-requires
-const rootMain = require('../../../.storybook/main');
-
-// Use the following syntax to add addons!
-// rootMain.addons.push('');
-rootMain.stories.push(...['../src/lib/**/*.stories.mdx', '../src/lib/**/*.stories.@(js|jsx|ts|tsx)']);
-
-module.exports = rootMain;
--- libs/ui/.storybook/preview.js
+++
@@ -1,6 +0,0 @@
-import { withKnobs } from '@storybook/addon-knobs';
-import { addDecorator } from '@storybook/react';
-
-addDecorator(withKnobs);
-
-export const parameters = { layout: 'fullscreen' };
